e47cf117916e1ed34edbc43b274cec11d21b26c5
Gabriel Gama <gabu_gama@hotmail.com>
2019-02-15 06:42:23 -0200

    make it better

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.openUri("mongodb://prod:gbgamaprod10@ds135305.mlab.com:35305/heroku_60dl103q");
+mongoose.connect("mongodb://prod:gbgamaprod10@ds135305.mlab.com:35305/heroku_60dl103q");
 
 console.log("Connected to the database")
 
