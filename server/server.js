0c744ee573f21cb5d03b520a7c9bf81e3b422246
Gabriel Gama <gabu_gama@hotmail.com>
2019-02-15 06:04:17 -0200

    make it better

--- server/server.js
+++ server/server.js
@@ -12,12 +12,12 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
   );
-  res.header(
+  res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
